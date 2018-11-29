ed4be5fa473670935bde45c0bf4b62bfabd41815
ikhsant <39888845+ikhsant@users.noreply.github.com>
2018-11-29 07:44:05 +0700

    Initial commit

---
+++ application/controllers/User.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('Crud_model');
+	}
+
+	public function index()
+	{
+		// mengambil data user
+		$user = $this->Crud_model->select('users','*')->result();
+		// menyimpan ke array data
+		$data['user']	= $user;
+		$this->load->view('template/backend', $data, FALSE);
+	}
+
+}
+
+/* End of file User.php */
+/* Location: ./application/controllers/User.php */
